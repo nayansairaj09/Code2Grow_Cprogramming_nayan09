20b9f60ca51b409776551da8f7213c0bfed792db
nayansairaj09 <nayan_chintha@srmap.edu.in>
2025-10-26 13:23:02 +0530

    Add files via upload

--- day4_smart student grading.c
+++ day4_smart student grading.c
@@ -11,33 +11,38 @@ int main()
     total_marks=sub1+sub2+sub3+sub4+sub5;
     total_percentage=(total_marks/500)*100;
     printf("total_percentage:%.2f%%\n",total_percentage);
-    if(total_percentage>=90 && attendance>=75)
+
+    if(attendance<75)
+    {
+        printf("fail for low attendence");
+    }
+    if(total_percentage>=90)
     {
         printf("grade: %c\n",grade='A+');
         printf("remarks=excellent");
 
     }
-    else if(total_percentage>=80 && total_percentage<90 && attendance>=75)
+    else if(total_percentage>=80 && total_percentage<90)
     {
         printf("grade: %c\n",grade='A');
         printf("remarks=very good");
     }
-    else if(total_percentage>=70 && total_percentage<80 && attendance>=75)
+    else if(total_percentage>=70 && total_percentage<80)
     {
         printf("%c",grade='B');
         printf("remarks=good");
     }
-    else if(total_percentage>=60 && total_percentage<70 && attendance>=75)
+    else if(total_percentage>=60 && total_percentage<70)
     {
         printf("%c\n",grade='C');
         printf("remarks=average");
     }
-    else if(total_percentage>=50 && total_percentage<60 && attendance>=75)
+    else if(total_percentage>=50 && total_percentage<60)  
     {
         printf("%c\n",grade='D');
         printf("remarks=pass");
     }
-    else if(total_percentage>=45 && total_percentage<50 && attendance>=90)
+    else if(total_percentage>=45 && total_percentage<50)
     {
         printf("%c\n",grade='D');
         printf("remarks=pass ");
